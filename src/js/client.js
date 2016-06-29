98481ef2df8b80fca0d6e9207e57f362ac1b3686
Spartano <torac.catalin@yahoo.it>
2016-06-29 23:12:14 +0200

    Add files via upload

---
+++ src/js/client.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./pages/Layout";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
---
+++ src/js/pages/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+    <div>Killer News</div>
+    );
+  }
+}
